29a4eabb43833ff2375e68983cbc2708e916ca3c
Carlos Oliveira <oslcarlos@gmail.com>
2021-06-05 00:21:24 -0300

    Criando Variavel

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2_CriandoVariaveis
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Execultado o projeto 2 - Criando Variaveis");
+
+            int idade;
+            idade = 32;
+            Console.WriteLine(idade);
+
+            idade = 10;
+            Console.WriteLine(idade);
+
+            idade = 10 + 5;
+            Console.WriteLine(idade);
+
+            idade = 10 + 5 * 2;
+            Console.WriteLine(idade);
+
+            idade = (10 + 5) * 2;
+            Console.WriteLine("Minha idade é " + idade + "!");
+
+            Console.WriteLine("Execução finalizada Tecle enter para sair");
+            Console.ReadLine();
+
+        }
+    }
+}
